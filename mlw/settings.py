51101615254acd2bf29860ceff17c7909893eca4
uzmovie <mywork200509@gmail.com>
2023-10-18 17:33:45 +0300

    asas
    sqsqsa

--- mlw/settings.py
+++ mlw/settings.py
@@ -31,10 +31,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY ='djkfdgfjgj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
