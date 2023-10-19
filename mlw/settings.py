e3ef1008be32a016e5ebd357b90349d2ba8bd9c2
uzmovie <mywork200509@gmail.com>
2023-10-19 10:39:50 +0300

    ddadad
    sasass

--- mlw/settings.py
+++ mlw/settings.py
@@ -163,14 +163,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = '/'
