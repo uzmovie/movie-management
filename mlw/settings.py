33de9804e3a019a4cb6adbca3f9e74e4a5319435
uzmovie <mywork200509@gmail.com>
2023-10-19 09:39:33 +0300

    kjj
    xxdf

--- mlw/settings.py
+++ mlw/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
