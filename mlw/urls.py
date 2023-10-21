f40a36c54ccc1702c8068b262f057d229b1aca63
uzmovie <mywork200509@gmail.com>
2023-10-21 13:59:48 +0500

    dada
    sas

--- mlw/urls.py
+++ mlw/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path('admin/', admin.site.urls),
+    path('darwin/', admin.site.urls),
     path('', include('movies.urls')),
     path('accounts/', include('users.urls')),
 ]
