7709aca1f7d43a0a44b1eba2ceac8e3f29feed8e
uzmovie <mywork200509@gmail.com>
2023-10-21 13:55:42 +0500

    dada
    asas

--- mlw/urls.py
+++ mlw/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
     path('accounts/', include('users.urls')),
